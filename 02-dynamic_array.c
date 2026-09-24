ef37a97ce740cb163b424d1c9302a5ff6fbaf06f
Gummalla vijayalakshmi <gummallavijayalashmi84@gmail.com>
2026-09-24 09:04:20 +0530

    Initial commit for Hacker_rank programs

---
+++ 02-dynamic_array.c
@@ -0,0 +1,139 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+int parse_int(char*);
+
+int* dynamicArray(int n, int queries_rows, int queries_columns, int** queries, int* result_count) {
+    int** arr = (int**)malloc(n * sizeof(int*));
+    int* sizes = (int*)calloc(n, sizeof(int));
+    int* capacities = (int*)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        capacities[i] = 2; 
+        arr[i] = (int*)malloc(capacities[i] * sizeof(int));
+    }
+
+    int* result = (int*)malloc(queries_rows * sizeof(int));
+    *result_count = 0;
+    int lastAnswer = 0;
+
+    for (int i = 0; i < queries_rows; i++) {
+        int type = queries[i][0];
+        int x = queries[i][1];
+        int y = queries[i][2];
+
+        int idx = (x ^ lastAnswer) % n;
+
+        if (type == 1) {
+            if (sizes[idx] == capacities[idx]) {
+                capacities[idx] *= 2;
+                arr[idx] = (int*)realloc(arr[idx], capacities[idx] * sizeof(int));
+            }
+            arr[idx][sizes[idx]++] = y;
+        } else if (type == 2) {
+            int pos = y % sizes[idx];
+            lastAnswer = arr[idx][pos];
+            result[(*result_count)++] = lastAnswer;
+        }
+    }
+
+    // Free temporary arrays
+    for (int i = 0; i < n; i++) {
+        free(arr[i]);
+    }
+    free(arr);
+    free(sizes);
+    free(capacities);
+
+    return result;
+}
+
+int main() {
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char** first_multiple_input = split_string(rtrim(readline()));
+    int n = parse_int(first_multiple_input[0]);
+    int q = parse_int(first_multiple_input[1]);
+
+    int** queries = malloc(q * sizeof(int*));
+    for (int i = 0; i < q; i++) {
+        queries[i] = malloc(3 * sizeof(int));
+        char** queries_item_temp = split_string(rtrim(readline()));
+        for (int j = 0; j < 3; j++) {
+            queries[i][j] = parse_int(queries_item_temp[j]);
+        }
+    }
+
+    int result_count;
+    int* result = dynamicArray(n, q, 3, queries, &result_count);
+
+    for (int i = 0; i < result_count; i++) {
+        fprintf(fptr, "%d\n", result[i]);
+    }
+
+    fclose(fptr);
+    return 0;
+}
+
+/* Utility functions */
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+        if (!line) break;
+        data_length += strlen(cursor);
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') break;
+        alloc_length <<= 1;
+        data = realloc(data, alloc_length);
+    }
+
+    if (data[data_length - 1] == '\n') data[data_length - 1] = '\0';
+    return data;
+}
+
+char* ltrim(char* str) {
+    while (*str != '\0' && isspace(*str)) str++;
+    return str;
+}
+
+char* rtrim(char* str) {
+    char* end = str + strlen(str) - 1;
+    while (end >= str && isspace(*end)) end--;
+    *(end + 1) = '\0';
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+    int spaces = 0;
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+        splits[spaces - 1] = token;
+        token = strtok(NULL, " ");
+    }
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+    if (endptr == str || *endptr != '\0') exit(EXIT_FAILURE);
+    return value;
+}
